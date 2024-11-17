e3e130fa1f3a9553aa17b4520bb1cf2ffd34eae4
Thanchida <thanchida.a@ku.th>
2024-11-17 20:38:32 +0700

    Update model

--- nonprofit/models.py
+++ nonprofit/models.py
@@ -39,10 +39,26 @@ class Skill(models.Model):
 class Volunteer(models.Model):
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
-    skills = models.ManyToManyField(Skill)
 
     def __str__(self):
         return self.name
+
+
+# class VolunteerSkill(models.Model):
+#     volunteer = models.ForeignKey(Volunteer, on_delete=models.CASCADE)
+#     skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
+#
+#     def __str__(self):
+#         return f"{self.volunteer} - {self.skill}"
+class VolunteerSkill(models.Model):
+    volunteer = models.ForeignKey(Volunteer, on_delete=models.CASCADE)
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('volunteer', 'skill')
+
+    def __str__(self):
+        return f'{self.volunteer.name} - {self.skill.name}'
 
 
 class Participation(models.Model):
@@ -61,12 +77,11 @@ class EventForm(forms.ModelForm):
 
 
 class VolunteerForm(forms.ModelForm):
+    skills = forms.ModelMultipleChoiceField(queryset=Skill.objects.all(), widget=forms.CheckboxSelectMultiple)
+
     class Meta:
         model = Volunteer
         fields = ['name', 'email', 'skills']
-        widgets = {
-            'skills': forms.CheckboxSelectMultiple,
-        }
 
 
 class SkillForm(forms.ModelForm):
@@ -91,3 +106,14 @@ class ParticipationForm(forms.ModelForm):
     class Meta:
         model = Participation
         fields = "__all__"
+
+
+# class VolunteerSkillForm(forms.ModelForm):
+#     class Meta:
+#         model = VolunteerSkill
+#         fields = ['volunteer', 'skill']
+#         widgets = {
+#             'skill': forms.CheckboxSelectMultiple,
+#         }
+class VolunteerSkillForm(forms.Form):
+    skills = forms.ModelMultipleChoiceField(queryset=Skill.objects.all(), widget=forms.CheckboxSelectMultiple)
